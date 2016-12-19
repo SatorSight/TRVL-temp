648dfc619c30f4bb2bce95dc5cd03a3367aa7adb
Farengate <satorsight@gmail.com>
2016-12-19 21:25:16 +0300

    tests

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,7 +1,7 @@
 <?php
 
-//error_reporting(0);
-//ini_set('display_errors', 0);
+error_reporting(0);
+ini_set('display_errors', 0);
 
 $log = file_get_contents("php://input");
 //$log = $HTTP_RAW_POST_DATA;
