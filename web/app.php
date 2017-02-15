90225d40d9d75232f345c5a51688018ddbfc3457
Farengate <satorsight@gmail.com>
2017-02-15 15:38:45 +0300

    tests

--- web/app.php
+++ web/app.php
@@ -1,8 +1,8 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
-ini_set('display_errors', 1);
-//error_reporting(0);
-//ini_set('display_errors', 0);
+//error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
+//ini_set('display_errors', 1);
+error_reporting(0);
+ini_set('display_errors', 0);
 use Symfony\Component\HttpFoundation\Request;
 
 /**
