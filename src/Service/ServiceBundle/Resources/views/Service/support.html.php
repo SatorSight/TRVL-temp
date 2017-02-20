da6b7507894f53f8f4d2e6d94bf167aa4b816597
Farengate <satorsight@gmail.com>
2017-02-20 19:25:52 +0300

    tests

--- src/Service/ServiceBundle/Resources/views/Service/support.html.php
+++ src/Service/ServiceBundle/Resources/views/Service/support.html.php
@@ -1,6 +1,14 @@
+<style>body{width: 100%;
+        height: 100%;
+        background: #2a2a2a;
+        color: #eee;
+        font-size: 18px;
+        font-family: Arial, sans;}
+    a{color:inherit;}</style>
 
 <div style="text-align:center;">
     <img style="height: 100px;" src="http://style-you.ru/Icon-Small-40@3x.png">
+    <br>
     <br>
     Если у вас возникли вопросы или предложения по работе с приложением Traveltogether - пишите нам на почту <a href="mailto:traveltogether.ios@yandex.ru"> traveltogether.ios@yandex.ru </a>
 </div>
